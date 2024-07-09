bf5ad6c8a3351c0b35c69c7e13fc3f855db8e74f
OlevovaTB <150012176+OlevovaTB@users.noreply.github.com>
2024-07-09 16:20:34 +0300

    test for CD

---
+++ README.md
@@ -0,0 +1,15 @@
+# Protractor Selenium Teamcity Sample
+
+## Prerequisite
+* Install node-js and npm
+* Install dependencies `npm install`
+* Update `*.conf.js` files inside the `conf/` directory with your [LambdaTest Username and Access Key]
+
+## Running your tests
+* To run a single test, run `npm run single`
+* To run local tests, run `npm run local`
+* To run parallel tests, run `npm run parallel`
+
+## About LambdaTest
+
+[LambdaTest](https://www.lambdatest.com/) is a cloud based selenium grid infrastructure that can help you run automated cross browser compatibility tests on 2000+ different browser and operating system environments. LambdaTest supports all programming languages and frameworks that are supported with Selenium, and have easy integrations with all popular CI/CD platforms. It's a perfect solution to bring your [selenium automation testing](https://www.lambdatest.com/selenium-automation) to cloud based infrastructure that not only helps you increase your test coverage over multiple desktop and mobile browsers, but also allows you to cut down your test execution time by running tests on parallel.
---
+++ conf/local.conf.js
@@ -0,0 +1,42 @@
+// const {ltcapabilities} = require('../capabilities')
+
+username= process.env.LT_USERNAME || "ihor.bezeka",
+accessKey=  process.env.LT_ACCESS_KEY || "EaxeGHtAOKgDGJn4unvpTIo9UVib43mg3MXMHztSNP8M19lJ2E",
+
+exports.config = {
+  'specs': [ '../specs/local.js' ],
+
+  seleniumAddress: 'https://'+username+':'+accessKey+'@hub.lambdatest.com/wd/hub',
+
+  'capabilities': {
+    'build': 'protractor-selenium-sample',
+    'name': 'single-test',
+    'browserName': 'Safari',
+    'version':'17',
+    'platform': "macOS Sonoma",
+    'video': true,
+    'network': false,
+    'console': false,
+    'visual': false,
+    'screen_resolution': '1024x768',
+    'tunnel': true
+  },
+  onPrepare: () => {
+
+    myReporter = {
+        specStarted: function(result) {
+          specStr= result.id
+          spec_id = parseInt(specStr[specStr.length -1])
+          browser.getProcessedConfig().then(function (config) {
+            var fullName = config.specs[spec_id];
+            //var fileName = fullName.substring(fullName.lastIndexOf('/')+1);
+            browser.executeScript("lambda-name="+fullName.split(/(\\|\/)/g).pop())
+          });
+        }
+      };
+      jasmine.getEnv().addReporter(myReporter);
+  },
+  onComplete: () => {
+    browser.quit();
+  }
+};
